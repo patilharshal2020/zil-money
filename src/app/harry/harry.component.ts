27c9b09077d1267d299cf759bc98ca5ea35b0f90
harshal-patil <patilharshal675@gmail.com>
2022-10-12 17:07:45 +0530

    removed unused part

--- src/app/harry/harry.component.ts
+++ src/app/harry/harry.component.ts
@@ -6,22 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./harry.component.css']
 })
 export class HarryComponent implements OnInit {
-  // arr: any[] = [2, 4, 6, 8, 10];
-  str: string = 'harshal patil'
-  strRev: [] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    const str1 = this.str.split(' ');
-    const firstName = str1[0];
-    const lastName = str1[1];
-    this.str = `${lastName} ${firstName}`;
-    str1.reverse();
-    const str2 = str1;
-    console.log('str2', str2);
-    const replaced = str2.join().replace(',', ' ');
-    console.log('str2', replaced);
   }
 
 }
