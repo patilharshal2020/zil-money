626a9c1412ea5127124067af1b3523491fdc57fd
harshal-patil <patilharshal675@gmail.com>
2022-10-14 11:20:09 +0530

    code for even number in array

--- src/app/harry/harry.component.ts
+++ src/app/harry/harry.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./harry.component.css']
 })
 export class HarryComponent implements OnInit {
+  numList: number[] = [2,3,4,5,6];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const filteredList = this.numList.filter(x => x % 2 == 0);
+
+    console.log(filteredList);
   }
 
 }
