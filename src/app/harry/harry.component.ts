8906651fd53ecfc58bbd8aacc156e6512823a7e0
harshal-patil <patilharshal675@gmail.com>
2022-11-04 12:16:30 +0530

    added ui changes for media player

--- src/app/harry/harry.component.ts
+++ src/app/harry/harry.component.ts
@@ -6,17 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./harry.component.css']
 })
 export class HarryComponent implements OnInit {
+  showPlayer: boolean = true;
+  title: string = 'Harry Test Session:'
+  navs: string[] = [];
 
   currentTime: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+    this.navs = ['Q&A', 'Resources', 'People']
   } 
+
+  setActive(evt: any) {
+    console.log('evt', evt);
+  }
 
   
   onTimeUpdate(event: CustomEvent<number>) {
     this.currentTime = event.detail;
-    console.log('time', this.currentTime);
+    const roundTime = Math.round(this.currentTime);
+    console.log('time', roundTime);
+    if (roundTime == 5) {
+      this.currentTime += 60*8; 
+    }
   }
 
 }
